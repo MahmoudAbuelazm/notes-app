07f300105faab4dda9dcf5e7f54aa85fac94efa4
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2023-12-26 11:11:08 +0200

    app bar

---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(.05),
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
+      ),
+      child: Center(child: const Icon(Icons.search,
+      size: 28,)),
+    );
+  }
+}
